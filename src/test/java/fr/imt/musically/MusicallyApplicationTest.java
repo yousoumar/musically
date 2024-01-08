1f47362e69fa07a79f1212279dab93f936977277
yousoumar <oumaiyr@gmail.com>
2024-01-08 16:56:51 +0100

    chore: add context load test, rename song year field, disable sql logs

---
+++ src/test/java/fr/imt/musically/MusicallyApplicationTest.java
@@ -0,0 +1,13 @@
+package fr.imt.musically;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicallyApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
