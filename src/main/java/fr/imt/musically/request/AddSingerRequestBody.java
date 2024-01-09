737ffb531c8e5a03249d028281a6ae43ecccc1e2
yousoumar <oumaiyr@gmail.com>
2024-01-09 21:15:43 +0100

    fix: add singers to a song

--- src/main/java/fr/imt/musically/request/AddSingerRequestBody.java
+++ src/main/java/fr/imt/musically/request/AddSingerRequestBody.java
@@ -7,10 +7,13 @@ public class AddSingerRequestBody implements RequestBody {
 
     @NotNull
     @Pattern(regexp = "^[a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}$", message = "Singer id must be a valid UUID")
-    private final String singerId;
+    private String singerId;
 
     public AddSingerRequestBody(String singerId) {
         this.singerId = singerId;
+    }
+
+    public AddSingerRequestBody(){
     }
 
     public String getSingerId() {
